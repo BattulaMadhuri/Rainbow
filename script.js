66973bfaf6c276ddedef0cfe61e19511f0f824c7
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-22 09:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const color = ['VIOLET','INDIGO','BLUE','GREEN','YELLOW','ORANGE','RED'];
+body.style.backgroundColor='pink';
+document.addEventListener('click',function(){
+    const colorIndex=parseInt(Math.random()*color.length);
+    body.style.backgroundColor=color[colorIndex];
+    function number(){
+        const num=parseInt(Math.random()*VideoColorSpace,length);
+        document.getElementById()
+    }
+});
